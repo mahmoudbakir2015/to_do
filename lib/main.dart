1ae5cd112f5fdec37218fa70d066a71c38b894f4
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2025-08-26 20:46:17 +0300

    fix some error

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,19 @@ import 'package:to_do/layout/to_do_app/home_layout_to_do.dart';
 import 'package:to_do/shared/cubit/to_do_cubit/cubit.dart';
 import 'package:to_do/shared/cubit/to_do_cubit/states.dart';
 import 'package:to_do/shared/network/local/cache_helper.dart';
-import 'package:to_do/shared/network/remote/dio_helper.dart';
 import 'shared/observer_block.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await CacheHelper.init();
-  DioHelper.init();
   runApp(
     const MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
-
-  const MyApp( {super.key});
+  const MyApp({super.key});
 
   @override
   build(BuildContext context) {
